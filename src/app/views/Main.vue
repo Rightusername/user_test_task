d26843f52e0b549a83f68e84e5c7657a25fae211
Lime <dristk@gmail.com>
2020-08-14 18:50:14 +0300

    fix: add missing search feature

--- src/app/views/Main.vue
+++ src/app/views/Main.vue
@@ -4,23 +4,43 @@
       <h1>
         Users
       </h1>
-      <div class="primary-btn" @click="createUser">Create User</div>
+      <div class="header-controls">
+        <div class="search-wrap" :class="{ closed: !isOpenSearch }">
+          <div class="open-search" @click="openSearch">
+            <img src="./../../assets/images/search.svg" svg-inline alt="" />
+          </div>
+          <input
+            ref="searchInput"
+            v-model="searchQuery"
+            type="text"
+            class="t-input"
+            placeholder="Search by name"
+          />
+          <div class="close-search" @click="closeSearch">
+            <img src="./../../assets/images/cancel.svg" svg-inline alt="" />
+          </div>
+        </div>
+        <div class="primary-btn" @click="createUser">Create User</div>
+      </div>
     </header>
 
-    <div v-if="!users || users.length == 0" class="no-users">
+    <div v-if="!users || filteredUsers.length == 0" class="no-users">
       <span v-show="!usersLoaded">Loading...</span>
       <span v-show="usersLoaded">No users</span>
     </div>
     <div class="users-list">
-      <div v-for="item in users" :key="item.id" class="user">
+      <div v-for="item in filteredUsers" :key="item.id" class="user">
         <div class="controls">
           <div class="edit-user-btn" @click.stop.prevent="editUser(item)">
-            <img src="./../../assets/images/edit-button.svg" svg-inline alt="" />
+            <img
+              src="./../../assets/images/edit-button.svg"
+              svg-inline
+              alt=""
+            />
           </div>
           <div class="remove-user-btn" @click.stop.prevent="removeUser(item)">
             <img src="./../../assets/images/cancel.svg" svg-inline alt="" />
           </div>
-    
         </div>
         <h4>
           Name
@@ -43,7 +63,6 @@
 import { mapGetters } from 'vuex';
 import cloneDeep from 'lodash-es/clonedeep';
 
-
 import UserCreateModal from 'app/components/modals/UserCreateModal.vue';
 import UserEditModal from 'app/components/modals/UserEditModal.vue';
 import ConfrimModal from 'app/components/modals/ConfirmModal.vue';
@@ -54,10 +73,21 @@ export default {
   data() {
     return {
       usersLoaded: false,
+      searchQuery: '',
+
+      isOpenSearch: false,
     };
   },
   computed: {
     ...mapGetters(['users']),
+    filteredUsers() {
+      if (this.searchQuery.length == 0) {
+        return this.users;
+      } else {
+        let reg = new RegExp(this.searchQuery, 'gi');
+        return this.users.filter(user => user.name.match(reg));
+      }
+    },
   },
 
   created() {
@@ -115,6 +145,17 @@ export default {
         }
       );
     },
+
+    openSearch() {
+      this.isOpenSearch = true;
+      setTimeout(() => {
+        this.$refs.searchInput.focus();
+      }, 200);
+    },
+    closeSearch() {
+      this.isOpenSearch = false;
+      this.searchQuery = '';
+    },
   },
 };
 </script>
@@ -137,6 +178,77 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    .header-controls {
+      display: flex;
+      align-items: center;
+
+      .search-wrap {
+        margin-right: 20px;
+        position: relative;
+        input {
+          width: 200px;
+          padding: 7px 30px;
+          transition: 0.2s ease;
+        }
+
+        .open-search {
+          position: absolute;
+          cursor: pointer;
+          z-index: 1;
+          top: 0px;
+          left: 0px;
+          width: 30px;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          transition: 0.2s ease;
+
+          svg {
+            width: 16px;
+            height: auto;
+            path {
+              fill: rgb(136, 136, 136);
+            }
+          }
+        }
+
+        .close-search {
+          position: absolute;
+          cursor: pointer;
+          top: 0px;
+          right: 0px;
+          width: 30px;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          transition: 0.2s ease;
+
+          svg {
+            width: 10px;
+            height: auto;
+            path {
+              fill: rgb(136, 136, 136);
+            }
+          }
+        }
+
+        &.closed {
+          input {
+            opacity: 0;
+            width: 30px;
+            padding-right: 0;
+          }
+
+          .close-search {
+            opacity: 0;
+            pointer-events: none;
+          }
+        }
+      }
+    }
   }
 
   .no-users {
